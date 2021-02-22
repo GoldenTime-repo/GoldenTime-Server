65531d2b8cef5306e88781f54cc002ada38638f8
이재홍 <fleuve1008@gmail.com>
2021-02-23 04:09:30 +0900

    Add DB Schema

---
+++ config/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    timezone: '+09:00',
+    pool: {
+      max: 20,
+      min: 5,
+      idle: 60000,
+    },
+  },
+  production: {
+    username: process.env.PRODUCTION_DB_USERNAME,
+    password: process.env.PRODUCTION_DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.PRODUCTION_DB_PORT,
+    host: process.env.PRODUCTION_DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+    timezone: '+09:00',
+    pool: {
+      max: 20,
+      min: 5,
+      idle: 60000,
+    },
+  },
+};
