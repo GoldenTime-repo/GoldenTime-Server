e147ff4886ebc8f667a82503fd7350b3688b2bfd
이재홍 <fleuve1008@gmail.com>
2021-02-24 16:33:00 +0900

    Add UserInfo modify

--- controllers/authControllers/index.js
+++ controllers/authControllers/index.js
@@ -2,3 +2,4 @@ exports.signUp = require('./signUp');
 exports.signIn = require('./signIn');
 exports.userInfo = require('./userInfo');
 exports.signOut = require('./signOut');
+exports.modifiedUser = require('./modifiedUser');
